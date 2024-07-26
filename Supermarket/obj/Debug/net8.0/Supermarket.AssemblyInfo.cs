fd97fcef5ecb09c252df7d9601f5013e3acd8f67
Rahind M <rahindm@Rahinds-MacBook-Air.local>
2024-07-26 15:11:39 +0530

    workflow added

--- Supermarket/obj/Debug/net8.0/Supermarket.AssemblyInfo.cs
+++ Supermarket/obj/Debug/net8.0/Supermarket.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219b87b310527c319926f34c0970372832de7f6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
